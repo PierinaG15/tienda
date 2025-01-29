5e10c4f4dd836a925bfb13f5a31c886f7ccd7da5
PierinaG15 <pierinaagh7@gmail.comecho>
2025-01-29 19:40:26 +0100

    craando nueva carpeta de nueva_categoria

--- proveedor.php
+++ proveedor.php
@@ -1,12 +1,42 @@
 <?php
-if(! iseet($_GET["id"])){
+if (!isset($_GET["id"])) {
     header("Location: proveedores");
-    exit(); 
+    exit();
 }
 include("partials/cabecera.php");
+$sql = "SELECT P.id,P.nombre,P.web,D.id as iddireccion,D.calle,D.numero,D.comuna,D.ciudad
+,T.id as idtelefono,T.numero as telefono FROM tienda.proveedores P 
+left join tienda.direcciones D on P.id=D.proveedores_id
+left join tienda.telefonos T on T.idproveedores=P.id
+where P.id=:id;";
+$stm = $conexion->prepare($sql);
+$stm->bindParam(":id", $_GET["id"]);
+$result = $stm->execute();
 
 ?>
 <section>
+    <?php
+    $direcciones=[];
+    $telefonos=[];
+    $row=$stm->fetch(PDO::FETCH_ASSOC);
+    if($row) {
+        echo "<div class='proveedor'>
+        <h1>Proveedor</h1>
+        <h4>{$row['nombre']}</h4>
+        <p>Web: <a href='{$row['web']}'>{$row['web']}</a></p>
+        </div>";
+        if($row["iddireccion"]!=null){
+            $direcciones[] = [
+                "iddireccion" => htmlspecialchars($row["iddireccion"]),
+                "calle" => htmlspecialchars($row["calle"]),
+                "numero" => htmlspecialchars($row["numero"]),
+            ];
+        }
+    }
+var_dump($direcciones);
+exit();
+    ?>
+
 
 </section>
 <?php
